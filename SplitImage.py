53c90943ed0340d571bc1b73f530cd1de5831534
Haifeng Zhu <Haifeng.Zhu@activenetwork.com>
2019-05-09 17:10:51 +0800

    clear noise

---
+++ SplitImage.py
@@ -0,0 +1,83 @@
+#coding:utf-8
+
+from PIL import Image, ImageFilter, ImageDraw
+import numpy as np
+import sys,os
+
+IMG_PATH = "./"
+IMG_POSTFIX = ".png"
+
+def getImageByName(path, fileName):
+    imageFilePath = os.path.join(path, fileName + IMG_POSTFIX)
+    image = Image.open(imageFilePath)
+    return image
+
+#二值判断,如果确认是噪声,用改点的上面一个点的灰度进行替换
+#该函数也可以改成RGB判断的,具体看需求如何
+def getPixel(image,x,y,G,N):
+    L = image.getpixel((x,y))
+    if L > G:
+        L = True
+    else:
+        L = False
+ 
+    nearDots = 0
+    if L == (image.getpixel((x - 1,y - 1)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x - 1,y)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x - 1,y + 1)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x,y - 1)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x,y + 1)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x + 1,y - 1)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x + 1,y)) > G):
+        nearDots += 1
+    if L == (image.getpixel((x + 1,y + 1)) > G):
+        nearDots += 1
+    if nearDots < N:
+        return image.getpixel((x,y-1))
+    else:
+        return None
+ 
+# 降噪 
+# 根据一个点A的RGB值，与周围的8个点的RBG值比较，设定一个值N（0 <N <8），当A的RGB值与周围8个点的RGB相等数小于N时，此点为噪点 
+# G: Integer 图像二值化阀值 
+# N: Integer 降噪率 0 <N <8 
+# Z: Integer 降噪次数 
+# 输出 
+#  0：降噪成功 
+#  1：降噪失败 
+def clearNoise(image,G,N,Z):
+    draw = ImageDraw.Draw(image)
+    for i in range(0,Z):
+        for x in range(1,image.size[0] - 1):
+            for y in range(1,image.size[1] - 1):
+                color = getPixel(image,x,y,G,N)
+                if color != None:
+                    draw.point((x,y),color)
+
+def binarizing(img, threshold): #input: gray image
+    pixdata = img.load()
+    w, h = img.size
+    for y in range(h):
+        for x in range(w):
+            if pixdata[x, y] < threshold:
+                pixdata[x, y] = 0
+            else:
+                pixdata[x, y] = 255
+    return img
+
+
+if __name__ == '__main__':
+    print("begin")
+    image = getImageByName(IMG_PATH, "0032")
+    image = image.convert("L")
+    image = binarizing(image, 150)
+    clearNoise(image, 150, 4, 12)
+    # image = image.filter(ImageFilter.GaussianBlur(radius=1))  
+    # image = image.convert("1")
+    image.show()
